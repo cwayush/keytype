aa59be870f8c4fac971a6aa6a144dcd8c8e5dbd1
𝐀𝐲𝐮𝐬𝐡 𝐊𝐚𝐭𝐡𝐞𝐫𝐢𝐲𝐚 <imrealayushthakur@gmail.com>
2025-09-23 00:55:54 +0530

    run_on_local

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next';
 import { Geist_Mono } from 'next/font/google';
 import { Header } from '@/components/header';
 import './globals.css';
-import { Providers } from './provider';
 import { Toaster } from '@/ui/components/sonner';
+import { SessionProvider } from 'next-auth/react';
 
 const geistMono = Geist_Mono({
   subsets: ['latin'],
@@ -20,16 +20,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistMono.className} antialiased min-h-screen bg-gradient-to-b from-neutral-900 to-black text-neutral-400`}
-      >
-        <Providers>
+    <SessionProvider>
+      <html lang="en">
+        <body
+          className={`${geistMono.className} antialiased min-h-screen bg-gradient-to-b from-neutral-900 to-black text-neutral-400`}
+        >
           <Header />
           {children}
           <Toaster position="top-center" richColors />
-        </Providers>
-      </body>
-    </html>
+        </body>
+      </html>
+    </SessionProvider>
   );
 }
--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -43,7 +43,7 @@ export default auth((req) => {
 
 export const config = {
   matcher: [
-    '/((?!_next|api|trpc|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/(api|trpc)(.*)',
   ],
-  runtime: 'nodejs',
 };
